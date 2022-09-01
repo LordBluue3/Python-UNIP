9b216317176af0ee4aea6dad05e80f7464ebd7f5
LordBluue3 <58037508+LordBluue3@users.noreply.github.com>
2022-09-01 20:51:42 -0300

    Calculo de volume

---
+++ Exercice/exercice_1.py
@@ -0,0 +1,7 @@
+height = float(input("Digite a altura: "))
+width = float(input("Digite a largura: "))
+length = float(input("Digite o comprimento: "))
+
+volume = height * width * length
+
+print("O volume é: %.2f"%volume,"cm")
