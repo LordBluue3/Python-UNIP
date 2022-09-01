eec15c261fa6a5dbfc13c24b36e66fa413014910
LordBluue3 <58037508+LordBluue3@users.noreply.github.com>
2022-09-01 20:51:15 -0300

    Testando o if e elif

---
+++ conditionsAge.py
@@ -0,0 +1,10 @@
+age = int(input("Digite sua idade: "))
+
+if age <12:
+    print("criança")
+elif age <18:
+    print("adolescente")
+elif age<70:
+    print("adulto")
+elif age <=100:    
+    print("idoso")
