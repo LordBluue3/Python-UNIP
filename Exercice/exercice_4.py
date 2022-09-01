2b9129a6329547ecd2907bccf3b99f4d824aaa51
LordBluue3 <58037508+LordBluue3@users.noreply.github.com>
2022-09-01 20:44:49 -0300

    Exercicio mudando celsius para fahrenheit

---
+++ Exercice/exercice_4.py
@@ -0,0 +1,6 @@
+#Entrando com dados e salvando na variável
+celsius = float(input("Digite o valor da temperatura em graus Celsius: "))
+#Processamento
+fahrenheit = celsius*1.8+32
+#Saída
+print("Temperatura em fahrenheit",fahrenheit)
