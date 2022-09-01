d113c0bf469af2f4b73742b68f5195e8e3385fb8
LordBluue3 <58037508+LordBluue3@users.noreply.github.com>
2022-09-01 20:47:52 -0300

    Utilizando máscara na saida do print

---
+++ mask.py
@@ -0,0 +1,9 @@
+print("Entre com a largura")
+width = int(input())
+print("Entre com a altura")
+height = int(input())
+
+print("base do triangulo: %.3f"%width)
+print("altura do triangulo: %.2f"% height)
+
+print("%20f"%height)
