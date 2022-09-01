aaffd7035e241cbf8d7cb6e5ce80dabdfd7b8ea2
LordBluue3 <58037508+LordBluue3@users.noreply.github.com>
2022-09-01 20:52:43 -0300

    AVA, calculo de média

---
+++ AVA/main.py
@@ -0,0 +1,6 @@
+firstNumber = float(input("Digite o primeria nota: "))
+secondNumber = float(input("Digite o segunda nota: "))
+
+average = (secondNumber + firstNumber)/2
+
+print("Media = ",average)
